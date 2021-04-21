36c3f41e48630d14a50368a8c513b549c8af820b
ahnjaewon <ajw1259@gmail.com>
2021-04-21 12:14:54 +0900

    make MyRouter.js

--- MyServer.js
+++ MyServer.js
@@ -1,12 +1,19 @@
 const http = require('http');
+const url = require('url');
+
 const hostname = 'localhost';
 const port = 8000;
-const baseUrl = 'httl://' + hostname + ':' + 8000; 
+const baseUrl = 'http://' + hostname + ':' + 8000; 
 
-function start() {
+function start(route) {
     function onRequest(req, res){
+        let sBody = 'Hello, world! <br> I am in the cloud class.';
+
+        console.log('Request receive.');
+        pathname = new url.URL(req.url, baseUrl).pathname;
+        route(pathname);
         res.writeHead(200, {'content-Type': 'text/html'});
-        res.write('Hello world!');
+        res.write(sBody);
         res.end();
     }
 
